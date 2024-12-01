cea67353bb9cfdddcbcbd158d186157e3ee02b75
Adi Varshney <adi.varshney09@gmail.com>
2024-12-01 00:44:50 -0500

    Changing folders and paths.

---
+++ main.js
@@ -0,0 +1,19 @@
+import './modern-normalize.css';
+import './style.css';
+import './header.css';
+import './hero.css';
+import './about.css';
+import './featured.css';
+import './work.css';
+import './contact.css';
+import './footer.css';
+import './mobile-nav.css';
+import './utils.css';
+
+import mobileNav from './mobile-nav';
+import darkMode from './dark-mode';
+import lazyLoading from './lazy-loading';
+
+mobileNav();
+darkMode();
+lazyLoading();
